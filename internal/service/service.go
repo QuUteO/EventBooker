966132a7d2f81f4c6fe98a32248ae7d0acdb5cf4
QuUteO <quuteo86@gmail.com>
2026-07-29 12:23:08 +0300

    add: layer repository

--- internal/service/service.go
+++ internal/service/service.go
@@ -3,23 +3,25 @@ package service
 import (
 	"EventBooker/internal/model"
 	"context"
+
+	"github.com/google/uuid"
 )
 
 type Store interface {
 	// Users
 	CreateUser(ctx context.Context, user *model.User) error
-	GetUserByID(ctx context.Context, id int64) (*model.User, error)
+	GetUserByID(ctx context.Context, id uuid.UUID) (*model.User, error)
 	GetUserByEmail(ctx context.Context, email string) (*model.User, error)
 
 	// Events
 	CreateEvent(ctx context.Context, event *model.Event) error
-	GetEventByID(ctx context.Context, id int64) (*model.Event, error)
+	GetEventByID(ctx context.Context, id uuid.UUID) (*model.Event, error)
 	ListEvents(ctx context.Context) ([]*model.Event, error)
 
 	// Bookings
-	CreateBookingWithTx(ctx context.Context, booking *model.Booking) error
-	GetBookingByID(ctx context.Context, id int64) (*model.Booking, error)
-	UpdateBookingStatus(ctx context.Context, bookingID int64, status model.BookingStatus) error
-	ListBookingsByEventID(ctx context.Context, eventID int64) ([]*model.Booking, error)
+	CreateBooking(ctx context.Context, booking *model.Booking) error
+	GetBookingByID(ctx context.Context, id uuid.UUID) (*model.Booking, error)
+	UpdateBookingStatus(ctx context.Context, bookingID uuid.UUID, status model.BookingStatus) error
+	ListBookingsByEventID(ctx context.Context, eventID uuid.UUID) ([]*model.Booking, error)
 	CancelExpiredBookings(ctx context.Context) (int64, error)
 }
